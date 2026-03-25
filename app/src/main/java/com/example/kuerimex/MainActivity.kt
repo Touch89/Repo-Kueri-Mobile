cec036dc4746964a6edebe473c54dd330795940e
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2026-03-25 14:49:56 -0600

    Creación de NavBar inferior funcional
    Se creó la navbar de navegación inferior principal, así como se trasladó la pantalla de inicio a un fragmento separado para poder hacer el menú y los fragmentos vacíos de las otras vistas para ser creadas más tarde

--- app/src/main/java/com/example/kuerimex/MainActivity.kt
+++ app/src/main/java/com/example/kuerimex/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kuerimex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            val fragment = when(item.itemId){
+                R.id.home_fragment -> HomeFragment()
+                R.id.products_fragment -> ProductsFragment()
+                R.id.sales_fragment -> SalesFragment()
+                R.id.profile_fragment -> ProfileFragment()
+                else -> null
+            }
+
+            fragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame_container, it)
+                    .commit()
+            }
+
+            true
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frame_container, HomeFragment())
+                .commit()
         }
     }
 }
